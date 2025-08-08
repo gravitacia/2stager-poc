18976792192e50da9589a9d839bb15dc22b12faa
Gravity <137218519+gravitacia@users.noreply.github.com>
2025-08-08 15:08:48 +0400

    Update eicar.c

--- eicar.c
+++ eicar.c
@@ -1,16 +1,25 @@
+#include <windows.h>
 #include <stdio.h>
 
-int main() {
-    FILE *f = fopen("eicar_test_file.com", "w");
-    if (f == NULL) {
-        perror("failed to create");
-        return 1;
+#define EICAR_STRING "X5O!P%@AP[4\\PZX54(P^)7CC)7}$" \
+"EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*"
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+                      DWORD  ul_reason_for_call,
+                      LPVOID lpReserved) {
+    FILE *f;
+    switch (ul_reason_for_call) {
+        case DLL_PROCESS_ATTACH:
+            f = fopen("eicar.com", "w");
+            if (f) {
+                fprintf(f, EICAR_STRING);
+                fclose(f);
+            }
+            break;
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
     }
-
-    const char *eicar = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$"
-                        "EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*";
-    fprintf(f, "%s", eicar);
-    fclose(f);
-
-    return 0;
+    return TRUE;
 }
